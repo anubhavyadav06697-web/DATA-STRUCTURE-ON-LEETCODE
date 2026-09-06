c287569b02ac7f26b0a4a548d22c58131cf962a4
Anubhav Yadav <anubhavyadav06697@gmail.com>
2026-09-06 17:29:00 +0530

    Time: 2405 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select user_id , name , mail from Users
+where mail REGEXP '^[A-Za-z][A-Za-z0-9_./-]*@leetcode[.]com$'
+ AND mail LIKE BINARY '%@leetcode.com';
+
